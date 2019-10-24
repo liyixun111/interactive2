a6e0327fcc1e68c258d84c2a197337cd4bd8a04a
liyixun <liyixun@cca.edu>
2019-10-24 09:07:17 -0700

    changes

--- p2/js/p2.js
+++ p2/js/p2.js
@@ -53,9 +53,9 @@ squareA.addEventListener('mouseleave', function () {
 
 let squares1 = []
 
-for (let y = 0; y < 5; y++) {
+for (let y = 0; y < 10; y++) {
 
-    for (let x = 0; x < 10; x++) {
+    for (let x = 0; x < 20; x++) {
 
         let square1 = document.createElement('div')
         square1.classList.add('square1')
@@ -70,16 +70,16 @@ for (let y = 0; y < 5; y++) {
 
 }
 
-squareB.addEventListener('click', function () {
+squareB.addEventListener('mouseenter', function () {
     for (let counter = 0; counter < squares1.length; counter++) {
 
         let x = Math.random() * 400 - 150;
         let y = Math.random() * 200 - 100;
-        let s = Math.random() * .2;
+        let s = Math.random() * 1;
         let r = Math.random() * 360;
 
-        squares1[counter].style.transform = 'translate(' + x + 'px,' + y + 'px)' + 'scale(' + s + ')';
-        // squares[counter].style.transform = 'rotate(' + r + 'deg)';
+        // squares1[counter].style.transform = 'translate(' + x + 'px,' + y + 'px)' + 'scale(' + s + ')';
+        squares1[counter].style.transform = 'rotate(' + r + 'deg)' + 'translate(' + x + 'px,' + y + 'px)' + 'scale(' + s + ')';
         // squares[counter].style.transform = 'scale(' + s + ')';
         // squares[counter].style.transform = 'scale(' + s + ')' + 'rotate(' + r + 'deg)';
 
@@ -105,8 +105,8 @@ for (let y = 0; y < 5; y++) {
         let square2 = document.createElement('div')
         square2.classList.add('square2')
 
-        square2.style.top = y * 40 + 'px'
-        square2.style.left = x * 40 + 'px'
+        square2.style.top = y * 80 + 'px'
+        square2.style.left = x * 80 + 'px'
 
         squareC.appendChild(square2)
         squares2.push(square2)
