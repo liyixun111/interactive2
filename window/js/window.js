6f8bec550d68f5d4a167c87f4ac1292bf13c1f09
liyixun <liyixun@cca.edu>
2019-11-14 16:02:01 -0800

    changes

---
+++ window/js/window.js
@@ -0,0 +1,16 @@
+window.addEventListener('resize', pageResized)
+
+let box = document.querySelector('.box')
+
+function pageResized() {
+
+    let rotation = (1 - (window.innerWidth / window.screen.width)) * 10000
+    let percent = ((window.innerWidth / window.screen.width)) * 100
+    // let colorFromScreenWidth = 'hsl(0,0%,' + windowPercentOfScreen + '%)'
+    // document.body.style.color = colorFromScreenWidth
+    box.style.transform = 'scale(' + (percent / 100) + ') rotate(' + rotation + 'deg)'
+
+
+}
+
+pageResized()
